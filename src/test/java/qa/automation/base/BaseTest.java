79907fa9951f7c167758fd36768de3d206597f10
Munna kumar <48070541+munnakr643@users.noreply.github.com>
2024-07-21 19:30:50 +0530

    Update BaseTest.java

--- src/test/java/qa/automation/base/BaseTest.java
+++ src/test/java/qa/automation/base/BaseTest.java
@@ -39,55 +39,7 @@ public class BaseTest {
     public static Logger logger = LogManager.getLogger(BaseTest.class);
     ReadConfig readconfig = new ReadConfig();
 
-    public static String testId = null;
-    public static String appName=null;
-    public static String iosReset=null;
-    public static boolean zephyrEnv = flag("updateTest");
-    public static boolean slackNotification = flag("slack");
-
-
-    private final static String GLOBAL_PROPERTIES = "/src/main/resources/global.properties";
-    protected final static String TEST_DATA_PROPERTIES = "/src/main/resources/test-data.properties";
-    private final static String LOG4J_PROPERTIES = "./src/main/resources/log4j.properties";
-    private final static String START_LOGGING_TEXT = "------------------------------Start logging------------------------------";
-    private final static String SCREENSHOTS_DIR = System.getProperty("user.dir") + "/target/Screenshots/";
-
-    private static String deviceEnv = System.getenv("browserstack");
-    private static String userId = System.getenv("BROWSERSTACK_USERNAME");
-    private static String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-    static boolean browserstack = false;
-    public static final String USERNAME = userId;
-    public static final String AUTOMATE_KEY =accessKey ;
-    public static final String URL = "https://"+USERNAME+":"+AUTOMATE_KEY+"@hub-cloud.browserstack.com/wd/hub";
-    static{
-        if(deviceEnv != null && deviceEnv.equalsIgnoreCase("true")){
-            browserstack = true;
-        }
-    }
-    public static boolean jiraEnv = flag("createJira");
-
-    @BeforeMethod(alwaysRun = true)
-    public void baseTestBeforeMethod(){
-        File file = new File(SCREENSHOTS_DIR);
-        if (file.exists()) {
-            ScreenUtil.deleteFolder(file);
-        }
-
-    }
-
-
-    @BeforeMethod(alwaysRun = true)
-    public void beforeMethodBaseTest(Method method){
-    Test testClass =  method.getAnnotation(Test.class);
-        appName= testClass.groups()[0];
-    }
-
-    @BeforeMethod(alwaysRun = true)
-    public void beforeMethodBaseTestResetIos(Method method){
-        Test testClass =  method.getAnnotation(Test.class);
-       int index= testClass.groups().length;
-        iosReset=testClass.groups()[index-1];
-    }
+    
 
     /**
      * Initialises the appium Driver with its required capabilities
@@ -96,35 +48,6 @@ public class BaseTest {
      */
     public AppiumDriver<MobileElement> initAppiumDriver() throws IOException {
         AppiumDriver<MobileElement> mobileDriver;
-        logger = Logger.getLogger("PlobalApps");
-        PropertyConfigurator.configure(LOG4J_PROPERTIES);
-        logger.info(START_LOGGING_TEXT);
-        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + GLOBAL_PROPERTIES);
-        Properties prop = new Properties();
-        prop.load(fis);
-        String port = prop.getProperty("port");
-        String appiumServer = prop.getProperty("appiumServer");
-        logger.info("Port Number: " + port);
-        logger.info("Appium Server: " + appiumServer);
-        logger.info("App Name: " + appName);
-        if (browserstack){
-            DesiredCapabilities bsCapabilities =browserStackCapabilities();
-            if (System.getenv("platform").equalsIgnoreCase("Android")) {
-                mobileDriver = new AndroidDriver<>(new URL("http://hub.browserstack.com/wd/hub"), bsCapabilities);
-            }
-            else {
-                mobileDriver = new IOSDriver<>(new URL("http://hub.browserstack.com/wd/hub"), bsCapabilities);
-            }
-
-        }else {
-            DesiredCapabilities capabilities = capabilities();
-            if (System.getenv("platform").equalsIgnoreCase("Android"))
-                mobileDriver = new AndroidDriver<>(new URL("http://" + appiumServer + ":" + port + "/wd/hub"), capabilities);
-            else
-                mobileDriver = new IOSDriver<>(new URL("http://" + appiumServer + ":" + port + "/wd/hub"), capabilities);
-        }
-        mobileDriver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
-        ((HasSettings)mobileDriver).setSetting(Setting.WAIT_FOR_IDLE_TIMEOUT, 500);
         return mobileDriver;
     }
 
@@ -134,18 +57,7 @@ public class BaseTest {
      */
     public WebDriver initWebDriver() {
         WebDriver webDriver;
-        logger=Logger.getLogger("PlobalApps");
-        PropertyConfigurator.configure(LOG4J_PROPERTIES);
-        logger.info(START_LOGGING_TEXT);
-        io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
-        // TODO: Handle options for other Web Browsers
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        options.addArguments("window-size= 1400,800");
-//        options.addArguments("---headless");
-        webDriver = new ChromeDriver(options);
-        webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    
         return webDriver;
     }
 
@@ -174,30 +86,12 @@ public class BaseTest {
     }
 
     public void jiraCreationOnFailure( ITestResult result ) {
-        if (result.getStatus() == ITestResult.FAILURE) {
-            String issueSummary = "[Automation-bug] [" + readconfig.getPlatformName() + "] -" + result.getMethod().getConstructorOrMethod().getName()
-                    + " got failed due to " + result.getThrowable().getClass();
-            String issueDescription = testId+" : " + result.getMethod().getConstructorOrMethod().getName()
-                    + " got failed due to " + result.getThrowable().getClass();
-            if (jiraEnv) {
-                try {
-                    createJiraTicket("Bug", issueSummary + "!", issueDescription);
-                } catch (JsonProcessingException e) {
-                    throw new RuntimeException(e);
-                }
-            } else {
-                logger.info("Jira ticket creation is disabled as the 'createJira' flag is not set to 'true'.");
-            }
-        }
+    
     }
 
 
     public static void updateZephyrTestResult(ITestResult result){
-        if (zephyrEnv) {
-            ZephyrScaleUtil.updateTestResult(result);}
-        else {
-            logger.info("Zephyr Scale update test result is disabled as the 'updateTest' flag is not set to 'true'.");
-        }
+       
     }
 
 }
